2199b5e4bd6915658de3c6dac11e3b9cafb94940
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-01-18 17:47:39 +0900

    Initial Commit

---
+++ EasyCalculator/EasyCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyCalculatorApp.swift
+//  EasyCalculator
+//
+//  Created by 長橋和敏 on 2025/01/18.
+//
+
+import SwiftUI
+
+@main
+struct EasyCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
